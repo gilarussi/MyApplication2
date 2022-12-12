e7a5c913574acf2d4a6c29373b58829912a0c4aa
gilarussi <gil2005.ar@gmail.com>
2022-12-12 14:57:54 +0200

    right swipe work and email don't

--- app/src/main/java/com/example/myapplication2/MainActivity.java
+++ app/src/main/java/com/example/myapplication2/MainActivity.java
@@ -10,6 +10,17 @@ import android.widget.Toast;
 import com.lorentzos.flingswipe.SwipeFlingAdapterView;
 
 import java.util.ArrayList;
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -51,6 +62,56 @@ public class MainActivity extends AppCompatActivity {
             {
                 Toast.makeText(MainActivity.this, "Right is swiped", Toast.LENGTH_SHORT).show();
                 Log.d(TAG, "Right Swipe");
+
+                try {
+                String stringSenderEmail = "gil.arussi@boyar.org.il";
+                String stringReceiverEmail = "gil2005.ar@gmail.com";
+                String stringPasswordSenderEmail = "pomodoro";
+                String stringHost = "smtp.gmail.com";
+
+                Properties properties = System.getProperties();
+                properties.put("mail.smtp.host", stringHost);
+                properties.put("mail.smtp.port", "465");
+                properties.put("mail.smtp.ssl.enable", "true");
+                properties.put("mail.smtp.auth", "true");
+
+
+                javax.mail.Session session = Session.getInstance(properties, new Authenticator() {
+                    @Override
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(stringSenderEmail, stringPasswordSenderEmail);
+                    }
+                });
+
+
+                MimeMessage mimeMessage = new MimeMessage(session);
+
+                    mimeMessage.addRecipient(Message.RecipientType.TO, new InternetAddress(stringReceiverEmail));
+
+                    mimeMessage.setSubject("Android App Email");
+                    mimeMessage.setText("Hello, I want to work for you");
+
+                    Thread thread = new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+
+                            try {
+                                Transport.send(mimeMessage);
+                            } catch (MessagingException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    });
+
+                    thread.start();
+
+                } catch (AddressException e) {
+                    e.printStackTrace();
+                } catch (MessagingException e) {
+                    e.printStackTrace();
+                }
+
+
             }
 
             @Override
